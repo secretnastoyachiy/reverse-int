6a48e28a6b00a369b4c0b7918168861facc07cd1
almaskilichov-prog <itsmywaytoamerica@gmail.com>
2022-07-13 14:10:57 +0300

    feat: add reverse int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    const number = Math.abs(n);
+    let result = '';
+
+    for (let i = number.toString().length-1; i >= 0; i--) {
+        result = `${result}${number.toString()[i]}`;
+    }
+
+    result = Number(result);
+    return result;
 }
